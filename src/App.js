01b0df6f8a22bcc304494c7a300832f075149bbf
James Rushford <jar.rushford@gmail.com>
2022-06-26 13:16:39 -0600

    Fetch and display paginated list of breweries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [breweries, setBreweries] = useState([]);
+  const [currentPage, setCurrentPage ] = useState(1);
+  const [error, setError ] = useState('');
+
+  const getBreweries = async () => {
+    const url = `https://api.openbrewerydb.org/breweries?by_state=colorado&per_page=25&page=${currentPage}`
+    setError('');
+
+    try {
+      const response = await fetch(url);
+      const breweryList = await response.json();
+      if (breweryList.length === 0) {
+        setCurrentPage(currentPage - 1)
+      } else {
+        setBreweries(breweryList);
+      }
+    } catch(error) {
+      setError(error.message);
+    }
+  }
+
+  useEffect(() => {
+    getBreweries();
+  }, [currentPage])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Colorado Brewery List</h1>
+      { error && error }
+      {JSON.stringify(breweries)}
+      <footer>
+        <button onClick={() => {
+          if (currentPage >1) {
+            setCurrentPage(currentPage - 1)
+          }
+        }}> Back </button>
+        <span>{currentPage}</span>
+        <button onClick={() => {
+            setCurrentPage(currentPage + 1)
+        }}> Forward </button>
+      </footer>
     </div>
   );
 }
